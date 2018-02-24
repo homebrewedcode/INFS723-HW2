d3e65374967dad41fcf689eaf3591a309d22f1b2
Dale Pollitt <dale.negativezero@gmail.com>
2018-02-24 10:44:25 -0800

    create plain.txt file after deciphering cipher.txt

--- decrypt_file.rb
+++ decrypt_file.rb
@@ -8,8 +8,10 @@ decipher.key = "\x02\x94\xE7\x14\x3C\x2D\xF1\x35\xDA\xEF\xE9\xD7\x4D\xF8\xBD\xCC
 decipher.iv = "\xF3\xBC\x6E\x5B\x28\x1E\xBF\x67\x21\x0C\xD6\x88\x37\xFF\xDE\x9A"
 
 # Read in the encrypted file contents, decrypt the value,
-# then print it out to the screen.  Note that decipher.final part is required
+# then print it out to the screen as well as write to file.
+# Note that decipher.final part is required
 # to handle the necessary padding in the encryption format we are decrypting.
 contents = IO.binread('cipher.txt')
 plain = decipher.update(contents) + decipher.final
+File.open("plain.txt", "w") {|f| f.write plain}
 puts plain
